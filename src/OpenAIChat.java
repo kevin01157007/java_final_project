f3297f04512d4ed7b41083ddc4a8f64866d2e46d
AllenHung123 <allen45801885@gmail.com>
2024-05-21 20:53:48 +0800

    1

--- src/OpenAIChat.java
+++ src/OpenAIChat.java
@@ -16,7 +16,7 @@ public class OpenAIChat {
             // 手動構建 JSON 陣列
             message = "各位同學好：海洋環境與生態研究所　暑期海洋研究體驗活動目的:本所舉辦暑期大學生海洋研究體驗營的目的即在鼓勵在校學生認識海洋環境變遷與生態議題。對象:參與對象為大學部學生，錄取名額為10-12名報名期限與申請結果：申請自即日起至2024年5月17日截止。申請結果將於2024年5月24日以電子郵件寄發通知並於本所網站公布。說明:1.2024年7月1日(一)至8月29日(四)，進入本所各研究室學習，為期兩個月。2.參與學生將安排新海研二號參訪以及本校小艇碼頭SUP立槳活動。3.計畫結束前，參與計畫學將研究成果製成海報，預計於8月29日(暫定)舉辦成果發表會。註：經實驗室指導教授認可後，7-8月學習期間每月可領取6,000元學習獎勵金。線上報名: https://docs.google.com/forms/d/e/1FAIpQLSeS1ND0eiOqPIrowHZrB4CrchJmjlUNWtdfznvsiQUOAAJ-1A/viewform 活動詳情: https://imee.ntou.edu.tw/p/406-1055-43895,r1035.php?Lang=zh-tw 承辦人：王麗真小姐";
             String jsonMessages = "[" +
-                    "{\"role\": \"system\", \"content\": \"" + "You are a college student studying in Taiwan now ， you need to answer the question and reply the email with chinese as you are a Taiwannese student." + "\"}," +
+                    "{\"role\": \"system\", \"content\": \"" + "I am a college student studying in Taiwan now ， you have act for me to reply the email with chinese as you are a Taiwanese student." + "\"}," +
                     "{\"role\": \"user\", \"content\": \"" +message+ "\"}" +
                     "]";
 
