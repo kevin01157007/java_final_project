a616d2eaab16d9807676488ff0330079f0c1348f
kevin01157007 <152252977+kevin01157007@users.noreply.github.com>
2024-06-14 11:17:04 +0800

    把api設定環境變數

--- src/main/java/AIAnalyze.java
+++ src/main/java/AIAnalyze.java
@@ -11,11 +11,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.function.Consumer;
-
+import io.github.cdimascio.dotenv.Dotenv;
 public class AIAnalyze {
     public static void OpenAIAnalyze(String message, int i, Consumer<String> responseHandler) throws Exception {
         try {
-            String apiKey = "sk-Kopp9IyshjfozvME8XcAJneLwVmgixUIhediY9EX1NysAofh";
+            Dotenv dotenv = Dotenv.load();
+            String apiKey = dotenv.get("API_KEY");
             String prompt = null;
             if (i == 1) {
                 prompt = "You are now my personal assistant. You need to help me analyze and summarize this message in the simplest terms possible with Traditional Chinese. The fewer words the better.";
